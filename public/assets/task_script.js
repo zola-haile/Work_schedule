54d5ee12ace158d3fcfdee1df2a5abb0e0f07e67
Zelalem <zhaile@nd.edu>
2025-05-21 20:15:44 -0400

    Commit before starting task addition

--- public/assets/task_script.js
+++ public/assets/task_script.js
@@ -5,6 +5,10 @@ document.addEventListener("DOMContentLoaded",()=>{
         event.preventDefault();
         document.querySelector("#cont_1").classList.remove('hidden_task');
         document.querySelector("#cont_2").classList.add('hidden_task')
+        document.querySelector("#task_form_1").classList.add('hidden_task');
+        document.querySelector("#task_form_2").classList.add('hidden_task');
+
+
     });
 
 
@@ -15,5 +19,40 @@ document.addEventListener("DOMContentLoaded",()=>{
 
         document.querySelector("#cont_1").classList.add('hidden_task');
         document.querySelector("#cont_2").classList.remove('hidden_task');
+        document.querySelector("#task_form_1").classList.add('hidden_task');
+        document.querySelector("#task_form_2").classList.add('hidden_task');
+
     })
+
+
+    const add_task1 = document.querySelector("#task1_add_button");
+    add_task1.addEventListener("click",(event)=>{
+        event.preventDefault();
+
+        document.querySelector("#cont_2").classList.add('hidden_task');
+        document.querySelector("#cont_1").classList.add('hidden_task');
+        document.querySelector("#task_form_1").classList.remove('hidden_task');
+        document.querySelector("#task_form_2").classList.add('hidden_task');
+
+
+
+    });
+
+    const add_task2 = document.querySelector("#task2_add_button");
+    add_task2.addEventListener("click",(event)=>{
+        event.preventDefault();
+
+        document.querySelector("#cont_2").classList.add('hidden_task');
+        document.querySelector("#cont_1").classList.add('hidden_task');
+        document.querySelector("#task_form_1").classList.add('hidden_task');
+        document.querySelector("#task_form_2").classList.remove('hidden_task');
+
+
+    });
+
+
+
+
+
+
 });
